f72e6f5dfbef4f95fb63292f1eb4ddbb73559dcb
Anderkops <and.celular@gmail.com>
2021-03-08 22:00:35 -0300

    Day 3: Finalizado a implementação do context

--- ReactJS/movit-next/src/pages/index.tsx
+++ ReactJS/movit-next/src/pages/index.tsx
@@ -3,9 +3,11 @@ import Head from 'next/head';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css';
 import { Countdown } from "../components/Countdown";
+
 
 export default function Home() {
   return (
@@ -23,7 +25,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
 
