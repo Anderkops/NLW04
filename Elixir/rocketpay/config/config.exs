ab577d41938cdc08edc5ef68b0f50ab07705697f
Anderkops <and.celular@gmail.com>
2021-03-12 23:17:17 -0300

    Day 1 (Elixir): Criado o Projeto

---
+++ Elixir/rocketpay/config/config.exs
@@ -0,0 +1,37 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :rocketpay, Rocketpay.Repo,
+  database: "rocketpay_repo",
+  username: "user",
+  password: "pass",
+  hostname: "localhost"
+
+config :rocketpay,
+  ecto_repos: [Rocketpay.Repo]
+
+# Configures the endpoint
+config :rocketpay, RocketpayWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "HOSnOEYE5vtg4Ae8Kt2dJzYXEF3fxhvYw5NdIjOxyuWSSgZpvm86YpOEolh0d2Dg",
+  render_errors: [view: RocketpayWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Rocketpay.PubSub,
+  live_view: [signing_salt: "mt/nmoAI"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
